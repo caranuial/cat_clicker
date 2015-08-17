1576b9db67e979b657ed2c874873228d3811c4a3
caranuial <caranuial@gmail.com>
2015-08-17 16:03:21 +0200

    Requirements 2
    finishing touch req. 2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
 $(document).ready(function(){
+	
+	var catOneName = "Kitie Me";
+	var catTwoName = "Kitie Too";
+	
+	var numberOfClicks = 0;
+	var $numberOfClicks = $('#number_of_clicks');
+	var numberOfClicks2 = 0;
+	var $numberOfClicks2 = $('#number_of_clicks2');
+	
+	var $floatingHeader1 = $('#floating_header1 span');
+	var $floatingHeader2 = $('#floating_header2 span');
+	
+	$floatingHeader1.text(catOneName);
+	$floatingHeader2.text(catTwoName);
+	
+	$('#cat_image').click(function() {
+		numberOfClicks = numberOfClicks + 1;
+		$numberOfClicks.text(numberOfClicks);
+	});
 
-var numberOfClicks = 0;
-var $numberOfClicks = $('#number_of_clicks');
-
-$('#cat_image').click(function() {
-	numberOfClicks = numberOfClicks + 1;
-	console.log( numberOfClicks );
-	$numberOfClicks.text(numberOfClicks);
-});
+	$('#cat_image2').click(function() {
+		numberOfClicks2 = numberOfClicks2 + 1;
+		console.log( numberOfClicks2 );
+		$numberOfClicks2.text(numberOfClicks2);
+	});
 });
 
